fb69427a8f04173cf1ad7c7a8e39a48131b620e8
giusepperosso <giusepperosso@hotmail.com>
2024-12-24 09:44:42 +0100

    centre the planet and create home.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';  // Path to your Sign Up component
-import { LoginComponent } from './login/login.component';      // Path to your Login component
+import { LoginComponent } from './login/login.component';   // Path to your Login component
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/signup', pathMatch: 'full' }  // Default to signup page
+  { path: '', component: HomeComponent }  // Default to signup page
 ];
