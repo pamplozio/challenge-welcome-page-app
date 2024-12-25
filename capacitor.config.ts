7b3db5a99215252739f75cae10ab246bee07b789
giusepperosso <giusepperosso@hotmail.com>
2024-12-25 09:33:01 +0100

    implement capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.welcomePageApp.myapp',
+  appName: 'welcome-page-app',
+  webDir: 'dist/welcome-page-app/browser',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent { }
+export class HomeComponent implements AfterViewInit {
+  
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.disableIframeScroll();
+  }
+
+  private disableIframeScroll() {
+    const iframe = document.querySelector('iframe');
+    if (iframe) {
+      iframe.addEventListener('wheel', (event) => event.preventDefault(), { passive: false });
+    }
+  }
+}
